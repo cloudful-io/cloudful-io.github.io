1d653f993d9efdb43f12b13682ece6138add8c23
Will Yip <will@cloudful.io>
2025-03-06 23:33:41 -0500

    Designed initial navigation with branded theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from "react";
+import AppNavbar from "./components/AppNavbar";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Products from "./pages/Products";
+import Services from "./pages/Services";
+
+
+//import PrivateRoute from "./PrivateRoute";
+ 
 function App() {
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: 'light',
+      primary: {
+        main: '#42a5f5'
+      }
+    },
+  });
+
+
   return (
-    <div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 123
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <AppNavbar/>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/services" element={<Services/>} />
+
+
+        {/* Protected Routes */}
+       
+
+        {/* Default Home Page */}
+        <Route path="/" element={<Home/>} />
+      </Routes>
+      </ThemeProvider>
+    </Router>
   );
 }
 
---
+++ src/components/AppNavbar.js
@@ -0,0 +1,122 @@
+import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import CloudIcon from '@mui/icons-material/Cloud';
+import { Link } from "react-router-dom";
+
+const AppNavbar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+    };
+    
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                <CloudIcon sx={{ display: { xs: 'none', md: 'flex', color: 'white'}, mr: 1 }} />
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'none', md: 'flex' },
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.1rem',
+                    color: 'white',
+                    textDecoration: 'none',
+                    }}
+                >
+                    Cloudful
+                </Typography>
+
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                    size="large"
+                    aria-label="Menu"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                    >
+                    <MenuIcon sx={{ display: { color: 'white'}}}/>
+                    </IconButton>
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{ display: { xs: 'block', md: 'none' } }}
+                    >
+                    <MenuItem key="About" component={Link} to="/about" onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center' }}>About</Typography>
+                    </MenuItem>
+                    <MenuItem key="About" component={Link} to="/contact" onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center' }}>Contact</Typography>
+                    </MenuItem>
+                    <MenuItem key="Products" component={Link} to="/products" onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center' }}>Products</Typography>
+                    </MenuItem>
+                    <MenuItem key="Services" component={Link} to="/services" onClick={handleCloseNavMenu}>
+                        <Typography sx={{ textAlign: 'center' }}>Services</Typography>
+                    </MenuItem>
+                    </Menu>
+                </Box>
+                <CloudIcon sx={{ display: { xs: 'flex', md: 'none', color: 'white' }, mr: 1 }} />
+                <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    href="#app-bar-with-responsive-menu"
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'flex', md: 'none' },
+                    flexGrow: 1,
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.1rem',
+                    color: 'white',
+                    textDecoration: 'none',
+                    }}
+                >
+                    Cloudful
+                </Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, justifyContent: 'flex-end' }}>
+                    <Button color="inherit" component={Link} to="/about" sx={{ my: 2, color: 'white', display: 'block' }}>About</Button>
+                    <Button color="inherit" component={Link} to="/contact" sx={{ my: 2, color: 'white', display: 'block' }}>Contact</Button>
+                    <Button color="inherit" component={Link} to="/products" sx={{ my: 2, color: 'white', display: 'block' }}>Products</Button>
+                    <Button color="inherit" component={Link} to="/services" sx={{ my: 2, color: 'white', display: 'block' }}>Services</Button>
+                </Box>
+                </Toolbar>
+            </Container>
+            </AppBar>
+
+    )  
+};
+
+export default AppNavbar;
